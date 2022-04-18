60af8a3a277664fe422e5d6e9c819cb594918a91
Pedro Rodrigues Souza <55005350+PedroRodriguesSouza@users.noreply.github.com>
2022-04-18 16:26:56 -0300

    Update matriz.h

--- matriz.h
+++ matriz.h
@@ -1,25 +1,26 @@
-//Código de definição: Matriz.h
+//CÃ³digo de definiÃ§Ã£o: Matriz.h
 
 #ifndef MATRIZ_H
 #define MATRIZ_H
 
-//Definição do tipo
+//DefiniÃ§Ã£o do tipo
 
 typedef struct
 {
-    int lin; //Número de linhas da matriz
-    int col; //Número de colunas da matriz
+    int lin; //NÃºmero de linhas da matriz
+    int col; //NÃºmero de colunas da matriz
     float **mat; //Matriz
 } Matriz;
 
 void CriaMatriz(Matriz &, int, int); //Cria matriz NxM dinamicamente - ponteiros
 void ImprimeMatriz(Matriz); //Imprime a matriz - Linha a linha
 void ApagaMatriz(Matriz &); //Apaga a matriz criada dinamicamnete
-void AtribuiValor(Matriz, int, int, float); //Atribui o valor float na posição int, int(linha, coluna)
-int Linhas(Matriz); //Retorna o número de linhas da matriz
-int Colunas(Matriz); //Retorna o número de colunas da matriz
+void AtribuiValor(Matriz, int, int, float); //Atribui o valor float na posiÃ§Ã£o int, int(linha, coluna)
+int Linhas(Matriz); //Retorna o nÃºmero de linhas da matriz
+int Colunas(Matriz); //Retorna o nÃºmero de colunas da matriz
 float SomaMatriz(Matriz); //Retorna a soma de todos os elementos da matriz
 float MaxMatriz(Matriz); //Retorna o maior valor de uma matriz
 void LinhaExata(Matriz); //Retorna todos os elementos da linha desejada
+void ColunaExata(Matriz); //Retorna todos os elementos da coluna desejada
 
 #endif
